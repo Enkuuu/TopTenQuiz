1e5e5c967572419f499c73905ed205015e5477a4
Enku <95542651+Enkuuu@users.noreply.github.com>
2022-01-29 10:10:53 -0800

    very great javascript
    i hate it

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 let topTenList = [
+    // "one",
+    // "two",
+    // "three",
+    // "four",
+    // "five",
+    // "six",
+    // "seven",
+    // "eight",
+    // "nine",
+    // "ten",
     "1",
     "2",
     "3",
@@ -12,5 +22,25 @@ let topTenList = [
 ];
 
 topTenList.forEach(function(topTen){
-
+    let container = document.querySelector('.topTenContainer');
+    let newEl = document.createElement('p')
+    newEl.id = "j"+topTen.replaceAll(" ", "");
+    newEl.innerHTML = topTen;
+    newEl.style.visibility = 'hidden';
+    container.append(newEl);
 });
+
+function submitAnswer(that){
+    let answer = that.value;
+    that.value = '';
+
+    if (topTenList.includes(answer)){
+        showAnswer(answer);
+    }
+
+}
+
+function showAnswer(answer){
+    let topTenParagraph = document.querySelector("#j" + answer.replaceAll(" ",""));
+    topTenParagraph.style.visibility = 'visible';
+}
